37cc414481415e8f34758517169df888c12197b4
sandikodev <androxoss@hotmail.com>
2023-08-07 18:36:19 +0700

    formater: 1st commit with husky `npx run lint:fix` pre-validation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,6 @@
-import { NextPage } from "next"
+import { NextPage } from 'next';
 
-const Page: NextPage = () =>{
-  return (
-    <>
-      ini halaman utama
-    </>
-  )
-}
-export default Page
+const Page: NextPage = () => {
+  return <>ini halaman utama</>;
+};
+export default Page;
